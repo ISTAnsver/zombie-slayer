07b208d4f19e8bbd2662eb789b16a480579ed6d3
ISTAnsver <spoom95@gmail.com>
2020-07-24 08:50:28 +0300

    Draw all using STI module

--- main.lua
+++ main.lua
@@ -5,6 +5,43 @@ function love.load()
     image = love.graphics.newImage("assets/characters/player.png")
     quad = love.graphics.newQuad(0, 0, sprite_sheet.sprite.width, sprite_sheet.sprite.height, image:getDimensions())
     map = sti("assets/levels/level_0.lua")
+    spawn = {}
+    for k, object in pairs(map.objects) do
+        if object.name == "player_spawn" then
+            spawn = object
+            break
+        end
+    end
+
+    local sprites = map:addCustomLayer("Sprites", 4)
+    local sw, sh = quad:getTextureDimensions()
+    sprites.player = {
+        sprite = image,
+        x = spawn.x,
+        y = spawn.y,
+        ox = 48 / 2,
+        oy = 64 / 2
+    }
+
+    sprites.draw = function(self)
+		love.graphics.draw(
+            self.player.sprite,
+            quad,
+			math.floor(self.player.x),
+			math.floor(self.player.y),
+			0,
+			1,
+			1,
+			self.player.ox,
+			self.player.oy
+		)
+
+		-- Temporarily draw a point at our location so we know
+		-- that our sprite is offset properly
+		love.graphics.setPointSize(5)
+		love.graphics.points(math.floor(self.player.x), math.floor(self.player.y))
+	end
+
 end
 
 function love.update(dt)
@@ -12,7 +49,5 @@ function love.update(dt)
 end
 
 function love.draw()
-    love.graphics.scale(2, 2)
     map:draw(40, 0, 2, 2)
-    love.graphics.draw(image, quad, 200, 200)
 end

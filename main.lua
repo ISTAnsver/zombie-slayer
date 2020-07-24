06b9bbd80541ef143036edfa832a82b6bf0df46a
ISTAnsver <spoom95@gmail.com>
2020-07-24 09:50:03 +0300

    Add WSDA basic contoller

--- main.lua
+++ main.lua
@@ -1,9 +1,11 @@
 local sti = require "modules/sti"
 
 function love.load()
-    sprite_sheet = { sprite = { width = 48, height = 64 }, columns_count = 4, rows_count = 4 }
+
+    mv_force_delta = 3; 
+
     image = love.graphics.newImage("assets/characters/player.png")
-    quad = love.graphics.newQuad(0, 0, sprite_sheet.sprite.width, sprite_sheet.sprite.height, image:getDimensions())
+    quad = love.graphics.newQuad(0, 0, 48, 64, image:getDimensions())
     map = sti("assets/levels/level_0.lua")
     spawn = {}
     for k, object in pairs(map.objects) do
@@ -20,8 +22,64 @@ function love.load()
         x = spawn.x,
         y = spawn.y,
         ox = 48 / 2,
-        oy = 64 / 2
+        oy = 64 / 2,
+        speed = {
+            x = 0,
+            y = 0
+        },
+        mv_force = {
+            x = 0,
+            y = 0,
+        }
     }
+
+    sprites.update = function(self, dt)
+        direction = {
+            horizontal = 0,
+            vertical = 0
+        }
+        if love.keyboard.isDown("w") then
+            if direction.horizontal == 0 then
+                direction.vertical = -1
+            else
+                direction.vertical = -0.5
+                direction.horizontal = direction.horizontal * 0.5
+            end
+        end
+        if love.keyboard.isDown("s") then
+            if direction.horizontal == 0 then
+                direction.vertical = 1
+            else
+                direction.vertical = 0.5
+                direction.horizontal = direction.horizontal * 0.5
+            end
+        end
+        if love.keyboard.isDown("d") then
+            if direction.vertical == 0 then
+                direction.horizontal = 1
+            else
+                direction.horizontal = 0.5
+                direction.vertical = direction.vertical * 0.5
+            end
+        end
+        if love.keyboard.isDown("a") then
+            if direction.vertical == 0 then
+                direction.horizontal = -1
+            else
+                direction.horizontal = -0.5
+                direction.vertical = direction.vertical * 0.5
+            end
+        end
+
+        self.player.mv_force.x = mv_force_delta * direction.horizontal
+        self.player.mv_force.y = mv_force_delta * direction.vertical
+
+        self.player.speed.x = self.player.speed.x + self.player.mv_force.x * dt
+        self.player.speed.y = self.player.speed.y + self.player.mv_force.y * dt
+
+        self.player.x = self.player.x + self.player.speed.x * dt
+        self.player.y = self.player.y + self.player.speed.y * dt
+    end
 
     sprites.draw = function(self)
 		love.graphics.draw(

536efbe95041df0327e8c8d36c42962cfbf3af6e
ISTAnsver <spoom95@gmail.com>
2020-08-08 17:43:56 +0300

    ZS-33 #comment Add reverse function for vec2

--- vec2/vec2.lua
+++ vec2/vec2.lua
@@ -5,9 +5,14 @@ function Vec2:new(o)
     setmetatable(o, self)
     self.__index = self
     self.__add = self.add
+    self.__unm = self.reverse
     return o
 end
 
+function Vec2:reverse()
+    return Vec2:new{ x = -self.x, y = -self.y }
+end
+
 function Vec2:add(vec)
-    return Vec2:new{ x = self.x + vec.x, y = self.x + vec.y }
+    return Vec2:new{ x = self.x + vec.x, y = self.y + vec.y }
 end

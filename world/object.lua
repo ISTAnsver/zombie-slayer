332dea1d0171ecf68dc379329a7ffccf66e93fc3
ISTAnsver <spoom95@gmail.com>
2020-08-23 21:55:01 +0300

    Add functions for work with forces on object

--- world/object.lua
+++ world/object.lua
@@ -6,3 +6,11 @@ function Object:new(o)
     self.__index = self
     return o
 end
+
+function Object:applyForce(vec2)
+    table.insert(self.forces, vec2)
+end
+
+function Object:popForces()
+    return table.unpack()
+end

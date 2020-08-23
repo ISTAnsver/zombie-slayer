e377a74fe214c83c36d14dda68e73921edbb8370
ISTAnsver <spoom95@gmail.com>
2020-08-23 21:58:17 +0300

    Create setMeter function for the world class

--- world/world.lua
+++ world/world.lua
@@ -3,10 +3,14 @@ require "world/object"
 World = {}
 
 function World:new(o)
-    o = o or { objects = {} }
+    o = o or { objects = {}, meter = 100 }
     setmetatable(o, self)
     self.__index = self
     return o
+end
+
+function World:setMeter(pixels)
+    self.meter = pixels
 end
 
 function World:createObject()
